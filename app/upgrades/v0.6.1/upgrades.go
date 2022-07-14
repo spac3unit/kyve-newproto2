d9930d5c9f1c8259070084a85ca24a8c6cf9450a
Maximilian Breithecker <72022235+mbreithecker@users.noreply.github.com>
2022-07-14 12:51:08 +0200

    v0.6.1 (#19)
    * fix: add missing params migration for CommissionChangeTime
    
    * chore: bump version to v0.6.1

--- app/upgrades/v0.6.1/upgrades.go
+++ app/upgrades/v0.6.1/upgrades.go
@@ -1,4 +1,4 @@
-package v0_6_0
+package v0_6_1
 
 import (
 	registrykeeper "github.com/KYVENetwork/chain/x/registry/keeper"
@@ -19,6 +19,8 @@ func createRedelegationParameters(registryKeeper *registrykeeper.Keeper, ctx sdk
 	registryKeeper.ParamStore().Set(ctx, types.KeyRedelegationCooldown, types.DefaultRedelegationCooldown)
 
 	registryKeeper.ParamStore().Set(ctx, types.KeyRedelegationMaxAmount, types.DefaultRedelegationMaxAmount)
+
+	registryKeeper.ParamStore().Set(ctx, types.KeyCommissionChangeTime, types.DefaultCommissionChangeTime)
 }
 
 func migratePools(registryKeeper *registrykeeper.Keeper, ctx sdk.Context) {
